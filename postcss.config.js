314c01578e01766ec687c98a4b1727e617ae2fa3
hujing <1107086473@qq.com>
2018-04-19 14:18:27 +0800

    fd

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-import')(),
-    // require('stylelint')(stylelintConfig),
-    require('postcss-cssnext')({
-      browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
-    })
-  ]
-}
